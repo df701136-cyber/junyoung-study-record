5e01464075a596b66e9a657174587deea2f19380
kwon junyoung <df701136@gmail.com>
2026-03-06 16:18:12 +0900

    변수와 자료형 복

--- c언어 복습/복습중.cpp
+++ c언어 복습/복습중.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>  
+#define MAX 100 // MAX라는 기호상수를 100으로 정의.
 
 
 int main() {
-	printf("깃허브 공부기록 업데이트하는 법 깨우치다!!");
+	int x = 11; // 정수형 자료형 int
+	double y = 2.4; // 실수형 자료형 double
+	char z = 'a'; // 문자형 자료형 char
+
+	printf("%d \n",x); // %d는 정수형 자료형을 출력할 때 사용하는 서식 문자.
+	printf("%.2lf \n",y); // %lf는 실수형 자료형을 출력할 때 사용하는 서식 문자
+	printf("%c \n",z); // %c는 문자형 자료형을 출력할 때 사용하는 서식 문자.
+
 	return 0;
 } 
 
